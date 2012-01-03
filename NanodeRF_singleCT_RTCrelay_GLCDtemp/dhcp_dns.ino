444315da010df13868f1a469104f037dd61c8522
Trystan <trystan@trystan-laptop.(none)>
2012-01-03 12:15:15 +0000

    New NanodeRF example: fetched time from http reply and broadcasts it to listening nodes such as emonglcd, relays temperature from emonglcd to emoncms

---
+++ NanodeRF_singleCT_RTCrelay_GLCDtemp/dhcp_dns.ino
@@ -0,0 +1,46 @@
+int dhcp_status = 0;
+int dns_status = 0;
+
+void dhcp_dns()
+{
+  //-----------------------------------------------------------------------------------
+  // Get DHCP address
+  // Putting DHCP setup and DNS lookup in the main loop allows for: 
+  // powering nanode before ethernet is connected
+  //-----------------------------------------------------------------------------------
+  if (ether.dhcpExpired()) dhcp_status = 0;    // if dhcp expired start request for new lease by changing status
+  
+  if (!dhcp_status){
+    wdt_disable();
+    dhcp_status = ether.dhcpSetup();           // DHCP setup
+    wdt_enable(WDTO_8S);
+    Serial.print("DHCP status: ");             // print
+    Serial.println(dhcp_status);               // dhcp status
+    
+    if (dhcp_status){                          // on success print out ip's
+      ether.printIp("IP:  ", ether.myip);
+      ether.printIp("GW:  ", ether.gwip);  
+      
+      static byte dnsip[] = {8,8,8,8};  
+      ether.copyIp(ether.dnsip, dnsip);
+      ether.printIp("DNS: ", ether.dnsip);
+      //ether.copyIp(ether.hisip, hisip);                             // un-comment for posting to static IP server (no domain name)
+      //dns_status = 1;                                               // un-comment for posting to static IP server (no domain name)            
+    } else { ethernet_error = 1; }  
+  }
+  
+  //-----------------------------------------------------------------------------------
+  // Get server address via DNS
+  //-----------------------------------------------------------------------------------
+  if (dhcp_status && !dns_status){
+    wdt_disable();
+    dns_status = ether.dnsLookup(website);    // Attempt DNS lookup
+    wdt_enable(WDTO_8S);
+    Serial.print("DNS status: ");             // print
+    Serial.println(dns_status);               // dns status
+    if (dns_status){
+      ether.printIp("SRV: ", ether.hisip);      // server ip
+    } else { ethernet_error = 1; }  
+  }
+
+}
